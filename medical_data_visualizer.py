94a05a5847bdc6ca467fd030823777aa6c567ba3
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2024-10-11 13:43:11 +1000

    wip

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -16,18 +16,17 @@ df[df[['cholesterol', 'gluc']] > 1] = 1
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])    
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
     df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index = False).size().rename(columns={'size':'total'})
-    
 
     # 7
-
+    long = sns.catplot(data=df_cat, x='variable', y='total', kind='box', hue='cardio')
 
 
     # 8
-    fig = None
+    fig = long
 
 
     # 9
@@ -38,10 +37,16 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+       (df['ap_lo'] <= df['ap_hi']) & 
+       (df['height'] >= df['height'].quantile(0.025)) & 
+       (df['height'] <= df['height'].quantile(0.975)) &
+       (df['weight'] >= df['weight'].quantile(0.025)) &
+       (df['weight'] <= df['weight'].quantile(0.975))
+       ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
     mask = None
@@ -59,6 +64,3 @@ def draw_heat_map():
     fig.savefig('heatmap.png')
     return fig
 
-print(
-    draw_cat_plot()
-)
